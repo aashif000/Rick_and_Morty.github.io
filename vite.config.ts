8b6a17a4d5de51f9956b2400d247278e7d8bb640
Mohammed Aashif <163309073+aashif000@users.noreply.github.com>
2024-12-29 23:04:41 +0530

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,19 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "https://github.com/aashif000/Rick_and_Morty.github.io.git",
+  // Set base URL based on environment mode (production or development)
+  base: "https://aashif000.github.io/rick_and_morty.github.io/",  // Default base for local development
+  
   server: {
-    host: "::",
-    port: 8080,
+    host: "::", // Accept connections from any IP address (useful for local network access)
+    port: 8080, // Your local server port
   },
+  
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(), // Only add componentTagger in development
   ].filter(Boolean),
+  
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
